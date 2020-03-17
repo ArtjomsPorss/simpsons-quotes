411fc2ef6a6ca9938aecd9c4e05d9a4902294439
Artyom Porsh <artjom.porsh@gmail.com>
2020-03-17 20:20:23 +0000

    Initial commit. Generated Spring Boot application using Spring Initializr.

---
+++ src/main/java/com/artjomporsh/simpsonsquotes/SimpsonsQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.artjomporsh.simpsonsquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpsonsQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpsonsQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artjomporsh/simpsonsquotes/SimpsonsQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artjomporsh.simpsonsquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpsonsQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
